82c88717d2eb0b54f82a9c597df371f13accbd0d
uroxic <uroxic@gmail.com>
2020-08-13 17:19:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ bot = hoshino.init()
 app = bot.asgi
 
 if __name__ == '__main__':
-    bot.run(use_reloader=False, loop=asyncio.get_event_loop())
+    bot.run(debug=False, use_reloader=False, loop=asyncio.get_event_loop())
